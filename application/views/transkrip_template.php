924479c9f78e231b58ac09eedc2121a0230663c0
Hufadz Izzudin <hufadz.domain@gmail.com>
2018-12-22 21:22:03 +0700

    pdf updated

--- application/views/transkrip_template.php
+++ application/views/transkrip_template.php
@@ -8,208 +8,222 @@
     <!-- Custom Css -->
     <link href="<?php echo base_url() ?>assets/css/style.min.css" rel="stylesheet"/>
     <style>
-    	.col-print-1 {width:8%;float:left;}
-    	.col-print-2 {width:16%;float:left;}
-    	.col-print-3 {width:25%;float:left;}
-		.col-print-4 {width:33%;float:left;}
-		.col-print-5 {width:42%;float:left;}
-		.col-print-6 {width:50%;float:left;}
-		.col-print-7 {width:58%;float:left;}
-		.col-print-8 {width:66%;float:left;}
-		.col-print-9 {width:75%;float:left;}
-		.col-print-10{width:83%;float:left;}
-		.col-print-11{width:92%;float:left;}
-		.col-print-12{width:100%;float:left;}
-    	.col-print-offset-1 {margin-left:8%}
-    	.col-print-offset-2 {margin-left:16%}
-    	.batas{width:100%;}
-    	.col-print-1,.col-print-2,
-    	.col-print-3,.col-print-4,
-    	.col-print-5,.col-print-6,
-    	.col-print-7,.col-print-8,
-    	.col-print-9,.col-print-10,
-    	.col-print-11,.col-print-12{position: relative;padding:0 10px;}
+    	body{font-size: 12px}
+    	.content{vertical-align: initial;padding-left: 10px}
+    	.batas{width:100%;margin: 15px 0;border-top: 1px solid #7f7f7f70;}
     	.p-l-0{padding-left: 0}
     	.sub{font-weight: normal;font-style: italic}
     	.bg-olive{background-color: #808000}
+    	.m-5{margin: 5px;}
     </style>
 </head>
 <body style="font-family: none;background-color: white">
 	<table width="100%">
 		<tr>
-			<td widht="1%">
+			<!-- <td widht="1%"> -->
 				<!-- <img src="http://localhost/skpi/assets/images/logo-lg.png" height="90px" style='margin-top:5px'> -->
-			</td>
-			<td align="center" style="margin-left: -20px">
-				<h4>
+			<!-- </td> -->
+			<td align="center" width="100%">
+				<h5>
 					KEMENTERIAN RISET, TEKNOLOGI, DAN PENDIDIKAN TINGGI<br>
 					UNIVERSITAS JEMBER <br>
 					<!-- dynamic jurusan nanti -->
 					<span>Program Studi Sistem Informasi</span>
-				</h4>
+				</h5>
 				<p style="font-size: 15px">
 					Jalan Kalimantan 37 Kampus Bumi Tegal Boto Jember 68121, Jawa Timur, Indonesia
 				</p>
 			</td>
-			<td width="10%"></td>
+			<!-- <td width="10%"></td> -->
 		</tr>
-		<!-- <tr>
-			<td align="left" width="100%">
+		<tr>
+			<td align="center">
+				<hr class="batas">
+				<h5 class="m-5">SURAT KETERANGAN PENDAMPING IJAZAH</h5>
+				<p style="font-size: 18px">
+					<span style="font-style:italic">Diploma Supplement</span><br>
+					<span>Nomor: xxx/SI/SKPI/2017</span>
+				</p>
+				<hr class="batas">
+			</td>
+		</tr>
+		<tr>
+			<td align="justify">
 				<p>
 					Surat Keterangan Pendamping Ijazah (SKPI) merupakan dokumen yang menyatakan kemampuan kerja, penguasaan pengetahuan, dan sikap/moral pemegangnya. SKPI disiapkan oleh Universitas Jember sesuai dengan Kerangka Kualifikasi Nasional Indonesia (KKNI) dan Konvensi UNESCO tentang pengakuan studi, ijazah, gelar pendidikan tinggi.
 				</p>
-				<p>
+				<p style="font-style: italic;">
 					A Diploma Supplement (DS) is a document that provides the information of work ability, master of knowledge and attitudes/morale of the holder. DS is prepared by the University of Jember based on the Indonesia Qualifications Framework (IQF) and the UNESCO Convention on the recognition of studies, diplomas and degrees of higher education.					
 				</p>
 			</td>
-		</tr> -->
+		</tr>
+		<tr>
+			<td align="justify">
+				<h6>1. IDENTITAS PEMEGANG SURAT KETERANGAN PENDAMPING IJAZAH<br>	
+				<span class="sub">1. Identity of the Holder</span></h6>
+			</td>
+		</tr>
 	</table>
-	<div class="row clearfix">
-		<div class="col-print-12" style="text-align: center">			
-			<div class="col-print-2">
-			</div>
-			<hr class="batas">
-			<div class="col-print-offset-2 col-print-8">
-				<h4>
-					SURAT KETERANGAN PENDAMPING IJAZAH
-				</h4>				
-				<p style="font-size: 18px">
-					<span style="font-style:italic">Diploma Supplement</span>
-				</p>
-			</div>
-			<hr class="batas">
-			<div class="col-print-12" style="text-align:justify">
-
-				<h5>1. IDENTITAS PEMEGANG SURAT KETERANGAN PENDAMPING IJAZAH<br>	
-				<span class="sub">1. Identity of the Holder</span></h5>
-
-				<div class="col-print-6 p-l-0">
-					<h5>
-						NAMA LENGKAP<br>
-						<span class="sub">Full Name</span>
-						<p class="bg-olive">SINTA EKA FRITRIYANTI</p>
-					</h5>
-					<h5>
-						TEMPAT, TANGGAL LAHIR<br>
-						<span class="sub">Place and Date of Birth</span>
-						<p class="bg-olive">PASURUAN, 26 FEBRUARI 1995</p>
-					</h5>
-					<h5>
-						NIM<br>
-						<span class="sub">Student Identification Number</span>
-						<p class="bg-olive">102410101054</p>
-					</h5>
-				</div>
-				<div class="col-print-6 p-l-0">
-					<h5>
-						TAHUN LULUS<br>
-						<span class="sub">Year of Completion</span>
-						<p class="bg-olive">2017</p>
-					</h5>
-					<h5>
-						NOMOR IJAZAH<br>
-						<span class="sub">Diploma Reference Number</span>
-						<p class="bg-olive">267/SI/S1/2017</p>
-					</h5>
-					<h5>
-						GELAR<br>
-						<span class="sub">Name of Qualification</span>
-						<p class="bg-olive">SARJANA KOMPUTER (S.KOM)</p>
-					</h5>
-				</div>
-
-				<h5>2. IDENTITAS PENYELENGGARA PROGRAM PENDIDIKAN<br>	
-				<span class="sub">2. Identity of the University</span></h5>
-
-				<div class="col-print-6 p-l-0">
-					<h5>
-						SK PENDIRIAN PERGURUAN TINGGI<br>
-						<span class="sub">Decree of University Establishment</span>
-						<p>SK Menteri PTIP No. 153 tahun 1964 tertanggal 9 Nopember 1964</p>
-					</h5>
-					<h5>
-						AKREDITASI PERGURUAN TINGGI<br>
-						<span class="sub">Accreditation of Institution</span>
-						<p>A</p>
-					</h5>
-					<h5>
-						FAKULTAS<br>
-						<span class="sub">Name of Qualification</span>
-						<p>PROGRAM STUDI SISTEM INFORMASI</p>
-					</h5>
-					<h5>
-						PROGRAM STUDI DAN AKREDITASI<br>
-						<span class="sub">Name of Qualification</span>
-						<p>SARJANA KOMPUTER (S.KOM)</p>
-					</h5>
-					<h5>
-						JENIS DAN JENJANG PENDIDIKAN<br>
-						<span class="sub">Name of Qualification</span>
-						<p>SARJANA KOMPUTER (S.KOM)</p>
-					</h5>
-					<h5>
-						JENJANG KUALIFIKASI SESUAI KKNI<br>
-						<span class="sub">Name of Qualification</span>
-						<p>SARJANA KOMPUTER (S.KOM)</p>
-					</h5>					
-				</div>
-				<div class="col-print-6 p-l-0">
-					<h5>
-						SK PENDIRIAN PERGURUAN TINGGI<br>
-						<span class="sub">Decree of University Establishment</span>
-					</h5>
+	<table width="100%">
+		<tr>
+			<td align="justify" width="50%" class="content">
+				<h6>
+					NAMA LENGKAP<br>
+					<span class="sub">Full Name</span>
+					<p class="bg-olive">SINTA EKA FRITRIYANTI</p>
+				</h6>
+				<h6>
+					TEMPAT, TANGGAL LAHIR<br>
+					<span class="sub">Place and Date of Birth</span>
+					<p class="bg-olive">PASURUAN, 26 FEBRUARI 1995</p>
+				</h6>
+				<h6>
+					NIM<br>
+					<span class="sub">Student Identification Number</span>
+					<p class="bg-olive">102410101054</p>
+				</h6>
+			</td>
+			<td align="justify" width="50%" class="content">
+				<h6>
+					TAHUN LULUS<br>
+					<span class="sub">Year of Completion</span>
+					<p class="bg-olive">2017</p>
+				</h6>
+				<h6>
+					NOMOR IJAZAH<br>
+					<span class="sub">Diploma Reference Number</span>
+					<p class="bg-olive">267/SI/S1/2017</p>
+				</h6>
+				<h6>
+					GELAR<br>
+					<span class="sub">Name of Qualification</span>
+					<p class="bg-olive">SARJANA KOMPUTER (S.KOM)</p>
+				</h6>
+			</td>
+		</tr>
+	</table>
+	<table width="100%">
+		<tr>
+			<td align="justify">
+				<h6>2. IDENTITAS PENYELENGGARA PROGRAM PENDIDIKAN<br>	
+				<span class="sub">2. Identity of the University</span></h6>
+			</td>
+		</tr>
+	</table>
+	<table width="100%">
+		<tr>
+			<td align="justify" width="50%" class="content">
+				<h6>
+					SK PENDIRIAN PERGURUAN TINGGI<br>
+					<span class="sub">Decree of University Establishment</span>
 					<p>SK Menteri PTIP No. 153 tahun 1964 tertanggal 9 Nopember 1964</p>
-					<h5>
-						AKREDITASI PERGURUAN TINGGI<br>
-						<span class="sub">Accreditation of Institution</span>
-						<p>A</p>
-					</h5>
-					<h5>
-						FAKULTAS<br>
-						<span class="sub">Name of Qualification</span>
-						<p>PROGRAM STUDI SISTEM INFORMASI</p>
-					</h5>
-					<h5>
-						PROGRAM STUDI DAN AKREDITASI<br>
-						<span class="sub">Name of Qualification</span>
-						<p>SARJANA KOMPUTER (S.KOM)</p>
-					</h5>
-					<h5>
-						JENIS DAN JENJANG PENDIDIKAN<br>
-						<span class="sub">Name of Qualification</span>
-						<p>SARJANA KOMPUTER (S.KOM)</p>
-					</h5>
-					<h5>
-						JENJANG KUALIFIKASI SESUAI KKNI<br>
-						<span class="sub">Name of Qualification</span>
-						<p>SARJANA KOMPUTER (S.KOM)</p>
-					</h5>					
-				</div>
-
-				<h5>3. KUALIFIKASI DAN HASIL CAPAIAN PEMBELAJARAN<br>	
-				<span class="sub">3. Qualification and Achieved Learning Outcomes</span></h5>
-
-				<div class="col-print-6 p-l-0">
-					<h5>
-						A. CAPAIAN PEMBELAJARAN
-					</h5>
-					<p>Sikap dan Tata Nilai</p>
-					<p>
-						Lulusan memiliki kepribadian sikap dan perilaku yang ditunjukkan dengan pengabdian kepada Tuhan Yang Maha Esa dan sikap keagamaan; menegakkan nilai-nilai kemanusiaan dalam menjalankan tugas berdasarkan agama, moral, dan etika; kegiatan yang berkaitan dengan etika akademik; kontribusi pada peningkatan kualias hidup masyarakat, bangsa, negara, dan peradaban berdasarkan Pancasila; kebanggaan, patriotisme, nasionalisme, dan tanggung jawab kepada bangsa dan negara; apresiasi terhadap keragaman budaya, pandangan, agama, dan keyakinan, serta anti plagiarisme; taat hukum dan disiplin dalam kehidupan bermasyarakat dan bernegara.  
-					</p>
-				</div>
-				<div class="col-print-6 p-l-0">
-					<h5>
-						A. Learning Outcome
-					</h5>					
-					<p>Attitude and Values</p>
-					<p>
-						The graduate has personality shown by: his/her devotion to God Almighty and religious attitude; upholding values of humanity in the line of duty based on religion, morals, and ethics; activities related to academic ethics; contribution for the improvement of life quality of the society, the nation, the state, and civilization based on Pancasila; pride, nationalism, and responsibility to the state and the nation; appreciation to the cultural diversity, views, religions, and beliefs, as well as awareness of being an anti-plagiarism; obedience of the law and discipline in the social and civil life.    
-					</p>
-				</div>			
-			</div>
-		</div>
-	</div>
+				</h6>
+				<h6>
+					AKREDITASI PERGURUAN TINGGI<br>
+					<span class="sub">Accreditation of Institution</span>
+					<p>A</p>
+				</h6>
+				<h6>
+					FAKULTAS<br>
+					<span class="sub">Name of Qualification</span>
+					<p>PROGRAM STUDI SISTEM INFORMASI</p>
+				</h6>
+				<h6>
+					PROGRAM STUDI DAN AKREDITASI<br>
+					<span class="sub">Name of Qualification</span>
+					<p>SARJANA KOMPUTER (S.KOM)</p>
+				</h6>
+				<h6>
+					JENIS DAN JENJANG PENDIDIKAN<br>
+					<span class="sub">Name of Qualification</span>
+					<p>SARJANA KOMPUTER (S.KOM)</p>
+				</h6>
+				<h6>
+					JENJANG KUALIFIKASI SESUAI KKNI<br>
+					<span class="sub">Name of Qualification</span>
+					<p>SARJANA KOMPUTER (S.KOM)</p>
+				</h6>				
+			</td>
+			<td align="justify" width="50%" class="content">
+				<h6>
+					SK PENDIRIAN PERGURUAN TINGGI<br>
+					<span class="sub">Decree of University Establishment</span>
+				</h6>
+				<p>SK Menteri PTIP No. 153 tahun 1964 tertanggal 9 Nopember 1964</p>
+				<h6>
+					AKREDITASI PERGURUAN TINGGI<br>
+					<span class="sub">Accreditation of Institution</span>
+					<p>A</p>
+				</h6>
+				<h6>
+					FAKULTAS<br>
+					<span class="sub">Name of Qualification</span>
+					<p>PROGRAM STUDI SISTEM INFORMASI</p>
+				</h6>
+				<h6>
+					PROGRAM STUDI DAN AKREDITASI<br>
+					<span class="sub">Name of Qualification</span>
+					<p>SARJANA KOMPUTER (S.KOM)</p>
+				</h6>
+				<h6>
+					JENIS DAN JENJANG PENDIDIKAN<br>
+					<span class="sub">Name of Qualification</span>
+					<p>SARJANA KOMPUTER (S.KOM)</p>
+				</h6>
+				<h6>
+					JENJANG KUALIFIKASI SESUAI KKNI<br>
+					<span class="sub">Name of Qualification</span>
+					<p>SARJANA KOMPUTER (S.KOM)</p>
+				</h6>				
+			</td>
+		</tr>
+	</table>
+	<table width="100%">
+		<tr>
+			<td align="justify">
+				<h6>3. KUALIFIKASI DAN HASIL CAPAIAN PEMBELAJARAN<br>	
+				<span class="sub">3. Qualification and Achieved Learning Outcomes</span></h6>
+			</td>
+		</tr>
+	</table>
+	<table width="100%">
+		<tr>
+			<td align="justify" width="50%" class="content">
+				<h6>A. CAPAIAN PEMBELAJARAN</h6>
+				<p>Sikap dan Tata Nilai</p>
+				<p>
+					Lulusan memiliki kepribadian sikap dan perilaku yang ditunjukkan dengan pengabdian kepada Tuhan Yang Maha Esa dan sikap keagamaan; menegakkan nilai-nilai kemanusiaan dalam menjalankan tugas berdasarkan agama, moral, dan etika; kegiatan yang berkaitan dengan etika akademik; kontribusi pada peningkatan kualias hidup masyarakat, bangsa, negara, dan peradaban berdasarkan Pancasila; kebanggaan, patriotisme, nasionalisme, dan tanggung jawab kepada bangsa dan negara; apresiasi terhadap keragaman budaya, pandangan, agama, dan keyakinan, serta anti plagiarisme; taat hukum dan disiplin dalam kehidupan bermasyarakat dan bernegara.  
+				</p>
+				<p>
+					Kinerja mahasiswa dalam pembelajaran untuk suatu mata kuliah dinyatakan dengan huruf dan angka yang mencerminkan kualitas kinerja. Huruf A setara dengan angka 4 (empat) berkategori istimewa, huruf AB setara dengan angka 3,5 (tiga koma lima) berkategori sangat baik, huruf B setara dengan angka 3 (tiga) berkategori baik, huruf BC setara dengan angka 2,5 (dua koma lima) berkategori cukup baik, huruf C setara dengan angka 2 (dua) berkategori cukup, huruf CD setara dengan angka 1,5 (satu koma lima) berkategori kurang, huruf D setara dengan angka 1 (satu) berkategori kurang, huruf DE setara dengan angka 0,5 (nol koma lima) berkategori kurang, huruf E setara dengan angka 0 (nol) setara berkategori sangat kurang.
+				</p>
+				<p>
+					Penyelesaian program pendidikan ditandai dengan ketuntasan capaian pembelajaran di setiap program studi dan sesuai dengan jenjang pendidikannya. Program diploma satu dengan beban belajar paling sedikit 36 sks, diselesaikan dalam rentang waktu 2-4 semester. Program diploma dua dengan beban belajar paling sedikit 36 sks, diselesaikan dalam rentang waktu 2-4 semester. Program diploma tiga dengan beban belajar paling sedikit 36 sks, diselesaikan dalam rentang waktu 2-4 semester. Program diploma empat dengan beban belajar paling sedikit 36 sks, diselesaikan dalam rentang waktu 2-4 semester. Program diploma satu dengan beban belajar paling sedikit 36 sks, diselesaikan dalam rentang waktu 2-4 semester.
+				</p>
+				<p>
+					Penerimaan mahasiswa didasarkan atas beberapa persyaratan dan prosedur serta proses seleksi yang tidak diskriminatif dan berkualitas. Skema seleksi dilakukan secara nasional dan lokal. Seleksi nasional melalui jalur SNMPTN (Seleksi Nasional Masuk Perguruan Tinggi Negeri) dan SBMPTN (Seleksi Bersama Masuk Perguruan Tinggi Negeri). Mekanisme Seleksi Lokal atau Mandiri yang dilakukan oleh Universitas. Peserta seleksi adalah lulusan pendidikan menengah atas atau yang sederajat dan lulus pada ujian masuk masing-masing perguruan tinggi. Kandidat mahasiswa S2 harus memiliki ijazah Sarjana (S1) atau yang sederajat dan lulus ujian seleksi masuk perguruan tinggi. Untuk S3, Mahasiswa harus memiliki ijazah S2 atau yang sederajat dan lulus ujian seleksi masuk.
+				</p>
+			</td>
+			<td align="justify" width="50%" class="content sub">
+				<h6>A. LEARNING OUTCOME</h6>
+				<p>Attitude and Values</p>
+				<p>
+					The graduate has personality shown by: his/her devotion to God Almighty and religious attitude; upholding values of humanity in the line of duty based on religion, morals, and ethics; activities related to academic ethics; contribution for the improvement of life quality of the society, the nation, the state, and civilization based on Pancasila; pride, nationalism, and responsibility to the state and the nation; appreciation to the cultural diversity, views, religions, and beliefs, as well as awareness of being an anti-plagiarism; obedience of the law and discipline in the social and civil life. bermasyarakat dan bernegara.
+				</p>
+				<p>
+					Students learning performance for one subject is marked in letters and numbers indicating the performance quality. Scaling Systems is determined as follows
+					tabel nangkene astaghfirullaah
+				</p>
+				<p>
+					A completion of a program is marked by a successful completeness of learning achievement in each study program in accordance with the level of education. Below is the period of completion.
+					tabel maneh ya Allaah
+				</p>
+				<p>
+					Student admission is based on requirements and procedures through undiscriminating and qualified selection process. The selection scheme is done nationally through SNMPTN (National Admission Selection for State University) and SBMPTN (National Admission Test for State University). Local Selection is conducted by the university. Eligibility for bachelor degree admission is specified to high school graduates. A bachelor degree is required for master program admission and a master degree is compulsory for doctorate program. Passing the university entrance test for the admission is prerequisite.  
+				</p>
+			</td>
+		</tr>
+	</table>
 
 	<!-- Bootstrap Core js -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
